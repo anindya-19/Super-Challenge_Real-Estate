3d1eefdcafae3a09e9ef20bf7e38f06d1216448c
Anindya Ganguly <anindyaganguly19@gmail.com>
2026-01-22 09:14:42 +0530

    first commit

---
+++ properties/placeholderPropertyObj.js
@@ -0,0 +1,7 @@
+const placeholderPropertyObj = {
+    area: '1 Joe Bloggs street',
+    priceGBP: 100000,
+    roomsM2: [16, 12, 6, 11, 5],
+    comment: 'This is the description',
+    image: 'placeholder.jpg'
+}
